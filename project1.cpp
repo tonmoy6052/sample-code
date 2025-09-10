ad1ecd1a2e5b95a906a930796b3393698a5115c9
Tonmoy Shaha <tonmoy3530saha@gmail.com>
2025-09-10 12:02:03 +0600

    first commit

---
+++ project1.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+#define ordered_set tree < int, null_type, less < int > , rb_tree_tag, tree_order_statistics_node_update >
+
+#define ll long long int
+#define lld long double
+#define mem(a,x) memset(a,x,sizeof(a))
+#define srt(a) sort(a.begin(), a.end())
+#define rev(a) reverse(a.begin(), a.end())
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define nl "\n"
+const int N = 2e5 + 5;
+const int MOD = 1000000007;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cout<<"Hello World\n";
+    return 0;
+}
+// BS,PS,BM,P2,P,BA,FO
